837c7fef51748be3666599f44c2fb216fc8b293f
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-09 19:01:11 +0300

    url model updated

--- src/models.py
+++ src/models.py
@@ -6,13 +6,13 @@ from src.database import Base
 
 
 class URL(Base):
-    """Class to init database table URL"""
+    """Class to create table 'urls' in database"""
 
     __tablename__ = "urls"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     short_url = Column(String, unique=True, index=True)
-    admin_url = Column(String, unique=True, index=True)
+    admin_key = Column(String, unique=True, index=True)
     target_url = Column(String, index=True)
     is_active = Column(Boolean, default=True)
     clicks = Column(Integer, default=0)
