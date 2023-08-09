00e2d7b6df1710adca4f1976ec267027835b8b63
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-09 12:45:49 +0300

    create db entry path added in project

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
 """src/main.py"""
 
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
+
+from src import schemas
 
 app = FastAPI()
+
+
+def raise_bad_request_msg(message):
+    """Function to raise HTTP Exceptions"""
+    raise HTTPException(status_code=400, detail=message)
 
 
 @app.get("/", name="Welcome page")
 def root():
     """Function that sends a welcome message"""
     return "Welcome to the URL shortener API :)"
+
+
+@app.post("/url", name="Create URL")
+def create_url(url: schemas.URLBase):
+    """Function to create entry in database"""
+    if not validators.url(url.target_url):
+        raise_bad_request_msg(message="Your URL is not valid")
+    return f"TODO: Create database entry for {url.target_url}"
