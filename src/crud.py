ad70046d197c22551f1ba275d79df2069ee9a598
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-09 16:15:28 +0300

    get_url_from_db function moved from main to crud module

--- src/crud.py
+++ src/crud.py
@@ -6,6 +6,7 @@ from src import models, schemas, short_url_gen
 
 
 def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
+    """Function create URL in database"""
     random_user_url = short_url_gen.create_random_url()
     random_admin_url = short_url_gen.create_random_url(length=8)
     db_url = models.URL(
@@ -16,3 +17,12 @@ def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
     db.refresh(db_url)
 
     return db_url
+
+
+def get_db_url_by_user_url(db: Session, user_url: str) -> models.URL:
+    """Function return URL from database by user URL"""
+    return (
+        db.query(models.URL)
+        .filter(models.URL.user_url == user_url, models.URL.is_active)
+        .first()
+    )
